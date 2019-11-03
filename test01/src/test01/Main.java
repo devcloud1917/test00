067b15519a9c0367070eaf9199caf6e160239832
Admin <Admin@ToshibaSatelliteS70B10Z>
2019-11-03 10:17:45 +0200

    added main

---
+++ test01/src/test01/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test01;
+
+/**
+ * @author Admin
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main " );
+	}
+
+}
